ebbc7af5a096920d01c90d91b1d4e017274973f2
whoisraa <raainvictus@gmail.com>
2021-10-27 16:07:10 +0700

    created a form

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3500;
+const user = {
+    name: ''
+};
+
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        name: 'Binar Academy'
+    });
+});
+
+app.post('/', (req, res) => {
+    user.name = req.body.name;
+    res.render('index', user);
+});
+
+app.listen(port, () => console.log(`The server has started on http://localhost:${port}`));
